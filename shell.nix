38c9b06af56d712495646e91062f8ab878e047bc
Florian Beeres <yuuki@protonmail.com>
2021-03-31 09:26:27 +0200

    update nix

--- shell.nix
+++ shell.nix
@@ -4,12 +4,20 @@ let
   nixpkgs = import sources.nixpkgs { };
 
 in
-nixpkgs.mkShell {
+nixpkgs.mkShell rec {
   buildInputs = with nixpkgs.haskellPackages; [
     ghcid
     ormolu
     hlint
     fast-tags
     stack
+    haskell-language-server
+
+    # https://github.com/commercialhaskell/stack/issues/2130
+    zlib
+    nixpkgs.pkgconfig
   ];
+
+  # https://github.com/commercialhaskell/stack/issues/2130
+  LD_LIBRARY_PATH = nixpkgs.lib.makeLibraryPath buildInputs;
 }
