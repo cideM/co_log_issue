ab147b6769b2c62b1da8484f22ce8628045cc8ef
Florian Beeres <yuuki@protonmail.com>
2021-03-31 09:27:26 +0200

    formatting

--- app/Main.hs
+++ app/Main.hs
@@ -1,18 +1,30 @@
-{-# LANGUAGE BlockArguments        #-}
-{-# LANGUAGE DeriveGeneric         #-}
-{-# LANGUAGE DerivingVia           #-}
-{-# LANGUAGE FlexibleContexts      #-}
-{-# LANGUAGE FlexibleInstances     #-}
-{-# LANGUAGE InstanceSigs          #-}
-{-# LANGUAGE MonoLocalBinds        #-}
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE MonoLocalBinds #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE NoImplicitPrelude     #-}
-{-# LANGUAGE OverloadedStrings     #-}
-{-# LANGUAGE PatternSynonyms       #-}
-{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 
-import Colog (HasLog (..), LogAction, Message, Msg (..), WithLog, cmap, liftLogAction, log,
-              pattern D, pattern I, richMessageAction, withLog)
+import Colog
+  ( HasLog (..),
+    LogAction,
+    Message,
+    Msg (..),
+    WithLog,
+    cmap,
+    liftLogAction,
+    log,
+    richMessageAction,
+    withLog,
+    pattern D,
+    pattern I,
+  )
 import qualified Data.Text.Lazy as TL
 import Relude
 import qualified Web.Scotty.Internal.Types as Scotty
@@ -39,9 +51,9 @@ newtype App a = App {unApp :: AppEnv App -> IO a}
     )
     via ReaderT (AppEnv App) IO
 
-handler
-    :: (WithLog (AppEnv m) Message (Scotty.ActionT TL.Text m), MonadIO m)
-    => Scotty.ActionT TL.Text m ()
+handler ::
+  (WithLog (AppEnv m) Message (Scotty.ActionT TL.Text m), MonadIO m) =>
+  Scotty.ActionT TL.Text m ()
 handler = withLog (cmap (\(msg :: Message) -> msg {msgText = "foo"})) $ do
   log I "Hi"
   Scotty.html $ mconcat ["<h1>Scotty, beam me up!</h1>"]
